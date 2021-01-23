439354009955448d01a122ec299510853d3aacda
Klint Kanopka <klint.kanopka@gmail.com>
2021-01-23 08:58:43 -0800

    added early stopping and loss logging

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import pandas as pd
 
 def em_step(optimizer, loader, model, loss_fn, device, max_iter=100, eps=1e-2, verbose=False):
     loss = 0
+    loss_log = []
     for i in range(max_iter):
         last_loss = loss
-        loss = 0
+        loss = n_samples = 0
         for batch, resp in loader:
             batch, resp = batch.to(device), resp.to(device)
 
@@ -20,15 +21,19 @@ def em_step(optimizer, loader, model, loss_fn, device, max_iter=100, eps=1e-2, v
             batch_loss.backward()
             optimizer.step()
             loss += batch_loss.item() * batch.shape[0]
+            n_samples += batch.shape[0]
+
+        loss = loss / n_samples
+        loss_log += [loss]
 
         if i > 0 and last_loss - loss < eps:
-            return i, loss
+            return i, loss, loss_log
         elif verbose:
             print('    iter: {0} | loss: {1}'.format(i, loss))
 
-    return i, loss
+    return i, loss, loss_log
 
-def write_output(model, path):
+def write_output(model, loss_log, path):
     person_dict = {}
     item_dict = {}
     for idx, p in enumerate(model.named_parameters()):
@@ -36,11 +41,12 @@ def write_output(model, path):
             person_dict[p[0]] = p[1].data.cpu().numpy()
         else:
             item_dict[p[0]] = p[1].data.cpu().numpy()
-    print(person_dict, item_dict)
     person_df = pd.DataFrame(person_dict)
     item_df = pd.DataFrame(item_dict)
+    loss_df = pd.DataFrame({'loss':loss_log})
     person_df.to_csv(path[:-4] + '_person_params.csv')
     item_df.to_csv(path[:-4] + '_item_params.csv')
+    loss_df.to_csv(path[:-4] + '_loss.csv')
 
 def main(input_path, irt_model, max_epochs, max_iter, 
             batch_size, num_workers, learning_rate_E, 
@@ -74,17 +80,28 @@ def main(input_path, irt_model, max_epochs, max_iter,
     e_opt = torch.optim.SGD(e_params)
     m_opt = torch.optim.SGD(m_params)
 
-    for epoch in range(max_epochs):
-        print('Epoch: {0}'.format(epoch))
+    epoch = 0
+    last_loss = 1e6 
+    loss = 1e5
+    loss_log = []
 
-        i, loss = em_step(e_opt, loader, model, loss_fn, device, max_iter, epsilon, verbose)
+    while epoch <= max_epochs and last_loss - loss > epsilon:
+        
+        print('Epoch: {0}'.format(epoch))
+        last_loss = loss
+
+        i, loss, log = em_step(e_opt, loader, model, loss_fn, device, max_iter, epsilon, verbose)
         print('  E-Step | Iter: {0} | Loss: {1}'.format(i, loss))
 
-        i, loss = em_step(m_opt, loader, model, loss_fn, device, max_iter, epsilon, verbose)
+        loss_log += log
+
+        i, loss, log = em_step(m_opt, loader, model, loss_fn, device, max_iter, epsilon, verbose)
         print('  M-Step | Iter: {0} | Loss: {1}'.format(i, loss))
 
+        loss_log += log
+        epoch += 1
 
-    write_output(model, input_path)
+    write_output(model, loss_log, input_path)
 
 if __name__ == '__main__':
     desc = 'Software for fitting an IRT mixture model for position effects'
@@ -97,7 +114,7 @@ if __name__ == '__main__':
     parser.add_argument('-w', '--num_workers', help='number of parallel workers for data loader', default=2)
     parser.add_argument('-E', '--learning_rate_E', help='E step learning rate', default=0.1)
     parser.add_argument('-M', '--learning_rate_M', help='M step learning rate', default=0.01)
-    parser.add_argument('-c', '--epsilon', help='epsilon for convergence', default=0.01)
+    parser.add_argument('-c', '--epsilon', help='epsilon for convergence', default=0.001)
     parser.add_argument('-v', '--verbose', help='print additional fitting information', action='store_true', default=False)
     args = parser.parse_args()
     main(args.input, args.irt_model, args.epochs, args.max_iterations, 
